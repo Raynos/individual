b21e8378e2cc03f960069571d960c5c1486d8f30
Raynos <raynos2@gmail.com>
2014-12-21 05:06:38 -0800

    implements one version

---
+++ one-version.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Individual = require('./index.js');
+
+module.exports = OneVersion;
+
+function OneVersion(key, version, defaultValue) {
+    var enforceKey = key + '_ENFORCE_SINGLETON';
+
+    var versionValue = Individual(enforceKey, version);
+
+    if (versionValue !== version) {
+        throw new Error('Can only have one copy of ev-store.\n' +
+            'You already have version ' + versionValue +
+            ' installed.\n' +
+            'This means you cannot install version ' + version);
+    }
+
+    return Individual(key, defaultValue);
+}

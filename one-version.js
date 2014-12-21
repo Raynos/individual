79389636d7d4702775c163438698627ba109b756
Raynos <raynos2@gmail.com>
2014-12-21 05:19:01 -0800

    pass in moduleName instead

--- one-version.js
+++ one-version.js
@@ -4,13 +4,15 @@ var Individual = require('./index.js');
 
 module.exports = OneVersion;
 
-function OneVersion(key, version, defaultValue) {
+function OneVersion(moduleName, version, defaultValue) {
+    var key = '__INDIVIDUAL_ONE_VERSION_' + moduleName;
     var enforceKey = key + '_ENFORCE_SINGLETON';
 
     var versionValue = Individual(enforceKey, version);
 
     if (versionValue !== version) {
-        throw new Error('Can only have one copy of ev-store.\n' +
+        throw new Error('Can only have one copy of ' +
+            moduleName + '.\n' +
             'You already have version ' + versionValue +
             ' installed.\n' +
             'This means you cannot install version ' + version);
